df75effc1558038fe79b048afcc17ea61c06156c
suketk <suketk@gmail.com>
2013-12-12 22:38:13 -0600

    i need to learn this

--- App/src/main/java/com/osacky/factor/MainActivity.java
+++ App/src/main/java/com/osacky/factor/MainActivity.java
@@ -39,40 +39,53 @@ public class MainActivity extends Activity {
         final TextView textProcessed = (TextView)findViewById(R.id.textNumbersProcessed);
         final TextView textFactorFound = (TextView)findViewById(R.id.textFactorFound);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v){
-                final Calendar t1 = Calendar.getInstance();
-                final long time1 = t1.getTimeInMillis();
-                Log.e("time1", "" + time1);
-                ParseQuery<ParseObject> query = ParseQuery.getQuery("TheNumber");
-                query.whereGreaterThan("threads", 0);
-                int primeNumber;
-                query.findInBackground(new FindCallback<ParseObject>() {
-                    public void done(List<ParseObject> primeList, ParseException e) {
-                        if (e == null) {
-                            Log.d("score", "Retrieved " + primeList.size() + " scores");
-                        } else {
-                            Log.d("score", "Error: " + e.getMessage());
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                ParseQuery<ParseObject> query = ParseQuery.getQuery(getString(R.string.parse_object));
+                query.whereGreaterThan(getString(R.string.parse_object_threads), 0);
+                query.findInBackground(new FindCallback<ParseObject>()
+                {
+                    public void done(List<ParseObject> primeList, ParseException e)
+                    {
+                        long prime_num;
+                        long prime_sqrt;
+
+                        Calendar t1 = Calendar.getInstance();
+                        long time1 = t1.getTimeInMillis();
+                        Log.d("time1", "" + time1);
+
+                        if(e!=null)
+                        {
+                            Log.d("Error", "Error: " + e.getMessage());
                         }
+
                         //IsPrime isPrime = new IsPrime();
                         if(primeList.isEmpty())
                         {
-                            int primeNumber = (int)((Math.random()+.2)*100000);
-                            ParseObject blah = new ParseObject("TheNumber");
-                            blah.put("number", primeNumber);
-                            blah.put("threads", 3);
-                            blah.saveInBackground();
-                            IsPrime.isPrime(primeNumber, 2, (int)Math.sqrt(primeNumber/4));
+                            prime_num = (long)((Math.random()+.2)*100000);
+                            prime_sqrt = (long) Math.ceil(Math.sqrt(prime_num));
+
+                            ParseObject create = new ParseObject(getString(R.string.parse_object));
+                            create.put(getString(R.string.parse_object_prime), prime_num);
+                            create.put(getString(R.string.parse_object_threads), 3);
+                            create.saveInBackground();
+
+                            IsPrime.isPrime(prime_num, 2, prime_sqrt);
                         }
                         else
                         {
-                            int primeNumber = primeList.get(0).getInt("number");
-                            int threads = 4-primeList.get(0).getInt("threads");
-                            primeList.get(0).increment("threads", -1);
-                            int primeSqrt = (int)Math.sqrt(primeNumber);
-                            IsPrime.isPrime(primeNumber, primeSqrt*threads/4, primeSqrt*(threads+1)/4);
+                            primeList.get(0).increment(getString(R.string.parse_object_threads), -1);
+
+                            prime_num = primeList.get(0).getLong(getString(R.string.parse_object_prime));
+                            prime_sqrt = (long) Math.ceil(Math.sqrt(prime_num));
+
+                            int threads = 4-primeList.get(0).getInt(getString(R.string.parse_object_threads));
+
+                            IsPrime.isPrime(prime_num, prime_sqrt*threads/4, prime_sqrt*(threads+1)/4);
                             primeList.get(0).saveInBackground();
-                            if(primeList.get(0).getInt("threads")<=0)
+                            if(primeList.get(0).getInt(getString(R.string.parse_object_threads))<=0)
                             {
                                 primeList.get(0).deleteInBackground();
                             }
@@ -90,7 +103,7 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
@@ -118,7 +131,7 @@ public class MainActivity extends Activity {
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
+                                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
         }
