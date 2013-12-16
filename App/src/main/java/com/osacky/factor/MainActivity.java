07df111e0b399b9e5fc519ffad55e283cf00941d
suketk <suketk@gmail.com>
2013-12-16 00:35:35 -0600

    Commented a bunch

--- App/src/main/java/com/osacky/factor/MainActivity.java
+++ App/src/main/java/com/osacky/factor/MainActivity.java
@@ -44,15 +44,20 @@ public class MainActivity extends Activity {
                         intent.getLongExtra(getString(R.string.factor), -1));
         }
 
+        /*
+        Take care of UI details after computation has completed.
+         */
         private void finish_up()
         {
             ProgressBar mProgress = (ProgressBar) findViewById(R.id.progressBar);
             mProgress.setProgress(100);
             Button button = (Button) findViewById(R.id.compute);
             button.setEnabled(true);
-//            button.setBackgroundColor(getResources().getColor(R.color.green));
         }
 
+        /*
+        Increment progress bar, and update relevant text fields.
+         */
         private void updateProgress(long num_proc, long time, long factor)
         {
             ProgressBar mProgress = (ProgressBar) findViewById(R.id.progressBar);
@@ -77,11 +82,10 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         myVib = (Vibrator) this.getSystemService(VIBRATOR_SERVICE);
 
-//        ParseObject.registerSubclass(Factors.class);
         Parse.initialize(this, "7A5hnUGC6Zxg6OQhZjWnB2gHAYc7h2VOOxYcVlMg", "L6tf0XRvzq6xSqbmf8EkMzJxqIfMMUqP5J9pljMC");
         ParseAnalytics.trackAppOpened(getIntent());
 
-        // The filter's action is BROADCAST_ACTION
+        //Register the broadcast receiver so we can dynamically update UI.
         IntentFilter mStatusIntentFilter = new IntentFilter(getString(R.string.broadcast_action));
         mStatusIntentFilter.addCategory(Intent.CATEGORY_DEFAULT);
         registerReceiver(new ResponseReceiver(), mStatusIntentFilter);
@@ -91,20 +95,18 @@ public class MainActivity extends Activity {
     {
         EditText thread_text = (EditText) findViewById(R.id.textThreads);
         EditText number_text = (EditText) findViewById(R.id.textNumber);
-        myVib.vibrate(50);
+        myVib.vibrate(50); //Haptic feedback
 
-        if(thread_text.getText().toString().length() <=0)
-        {
+        //Error handling toasts
+        if(thread_text.getText().toString().length() <=0){
             Toast toast = Toast.makeText(getApplicationContext(), getString(R.string.thread_lower_bound), 4);
             toast.show();
         }
-        else if(number_text.getText().toString().length() <=0)
-        {
+        else if(number_text.getText().toString().length() <=0){
             Toast toast = Toast.makeText(getApplicationContext(), getString(R.string.number_lower_bound), 4);
             toast.show();
         }
-        else if(number_text.getText().toString().length() > 19)
-        {
+        else if(number_text.getText().toString().length() > 19){
             Toast toast = Toast.makeText(getApplicationContext(), getString(R.string.number_upper_bound), 4);
             toast.show();
         }
@@ -113,40 +115,37 @@ public class MainActivity extends Activity {
             long num_threads = Long.parseLong(thread_text.getText().toString());
             long number = Long.parseLong(number_text.getText().toString());
 
-            if(num_threads==0)
-            {
+            if(num_threads==0){
                 Toast toast = Toast.makeText(getApplicationContext(), getString(R.string.thread_lower_bound), 4);
                 toast.show();
             }
-            else if(number < 1000000)
-            {
+            else if(number < 1000000){
                 Toast toast = Toast.makeText(getApplicationContext(), getString(R.string.number_lower_bound), 4);
                 toast.show();
             }
-            else if(number > Long.MAX_VALUE)
-            {
+            else if(number > Long.MAX_VALUE){
                 Toast toast = Toast.makeText(getApplicationContext(), getString(R.string.number_upper_bound), 4);
                 toast.show();
             }
-            else if(num_threads > 20)
-            {
+            else if(num_threads > 20){
                 Toast toast = Toast.makeText(getApplicationContext(), getString(R.string.thread_upper_bound), 4);
                 toast.show();
             }
             else
             {
+                //Check if number has already been created by querying for said number
                 ParseQuery<ParseObject> query = ParseQuery.getQuery(getString(R.string.parse_object));
                 query.whereEqualTo(getString(R.string.parse_object_number), number);
-                query.whereEqualTo(getString(R.string.parse_object_to_compute), true);
 
-                ParseObject computed = null;
+                ParseObject created = null;
                 try {
-                    computed = query.getFirst();
+                    created = query.getFirst();
                 } catch (ParseException e){
                     e.printStackTrace();
                 }
 
-                if(computed == null)
+                //If not created start background service to create.
+                if(created == null)
                 {
                     Log.e("null", "Should call intent.");
                     Intent mServiceIntent = new Intent(this, CreateService.class);
@@ -154,9 +153,8 @@ public class MainActivity extends Activity {
                     mServiceIntent.putExtra(getString(R.string.parse_object_threads), num_threads);
                     this.startService(mServiceIntent);
                 }
-                else
-                {
-                    Toast toast = Toast.makeText(getApplicationContext(), "Value already computed, press show result", 4);
+                else{
+                    Toast toast = Toast.makeText(getApplicationContext(), "Value already created, press show result", 4);
                     toast.show();
                 }
             }
@@ -165,13 +163,11 @@ public class MainActivity extends Activity {
 
     public void perform_computation(View v)
     {
-        EditText number_text = (EditText) findViewById(R.id.textNumber);
-        Button button = (Button) findViewById(R.id.compute);
-        button.setEnabled(false);
-//        button.setBackgroundColor(getResources().getColor(R.color.red));
+        myVib.vibrate(50);
+
+        //Set up UI details before computation starts
         ProgressBar mProgress = (ProgressBar) findViewById(R.id.progressBar);
         mProgress.setProgress(0);
-        myVib.vibrate(50);
 
         TextView textTime = (TextView) findViewById(R.id.textTime);
         TextView textProc = (TextView) findViewById(R.id.textNumbersProcessed);
@@ -180,6 +176,11 @@ public class MainActivity extends Activity {
         textProc.setText(getString(R.string.num_proc));
         textFactor.setText(getString(R.string.factor));
 
+        EditText number_text = (EditText) findViewById(R.id.textNumber);
+        Button button = (Button) findViewById(R.id.compute);
+        button.setEnabled(false);
+
+        //Create query to get information to start computation
         ParseQuery<ParseObject> query = ParseQuery.getQuery(getString(R.string.parse_object));
         query.whereEqualTo(getString(R.string.parse_object_to_compute), true);
 
@@ -190,15 +191,14 @@ public class MainActivity extends Activity {
             e.printStackTrace();
         }
 
-        if(computation==null)
-        {
+        if(computation==null){
             Toast toast = Toast.makeText(getApplicationContext(), "Nothing to compute, please create", 4);
             toast.show();
             button.setEnabled(true);
-//            button.setBackgroundColor(getResources().getColor(R.color.green));
         }
         else
         {
+            //Mark it as computed and save, so another thread does not solve the same sub-problem
             computation.put(getString(R.string.parse_object_to_compute), false);
             try {
                 computation.save();
@@ -212,6 +212,7 @@ public class MainActivity extends Activity {
 
             number_text.setText("" + number);
 
+            //Start background service to compute
             Intent mServiceIntent = new Intent(this, ComputationService.class);
             mServiceIntent.putExtra(getString(R.string.parse_object_number), number);
             mServiceIntent.putExtra(getString(R.string.parse_object_low), low);
@@ -224,6 +225,7 @@ public class MainActivity extends Activity {
         EditText number_text = (EditText) findViewById(R.id.textNumber);
         long number = Long.parseLong(number_text.getText().toString());
 
+        //Start activity to print list of results
         Intent show_results_intent = new Intent(this, ShowResults.class);
         show_results_intent.putExtra(getString(R.string.parse_factors_key), number);
         startActivity(show_results_intent);
